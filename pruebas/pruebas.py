22d4b7e3ce86a42b8a1ce107f35cca918b2f9fb3
mathias <cuki.mallqui.perro@gmail.com>
2025-04-20 11:20:13 -0500

    Commit limpio sin token

--- pruebas/pruebas.py
+++
@@ -1,9 +0,0 @@
-import time
-
-current_time = time.time()
-
-
-print(f"el tiempo actual {current_time}")
-
-formatted_time = time.strftime("%H:%M:%S", time.localtime())
-print(f"Hora actual: {formatted_time}")
